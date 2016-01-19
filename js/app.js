cb15dc03cb1dfa79b32e46e75ff684f6ea16da70
icarrera <irismcarrera@gmail.com>
2016-01-18 17:12:18 -0800

    three randomized images appear

--- js/app.js
+++ js/app.js
@@ -5,30 +5,46 @@ function Products (productName, filePath) {
 this.productName = productName;
 this.filePath = filePath;
 productImages.push(this);
-
 }
 
 productRandomizer = function () {
      return Math.floor(Math.random() * productImages.length);
 }
 
-var imageAppear = function(){
-  var x = productRandomizer();
-  var productImageOne = document.getElementById('imageOne');
-  productImageOne.src = productImages[x].filePath;
+var imageAppearOne = function(){
+    var x = productRandomizer();
+    var productImageOne = document.getElementById('imageOne');
+    productImageOne.src = productImages[x].filePath;
+}
+var imageAppearTwo = function() {
+    var y = productRandomizer();
+    var productImageTwo = document.getElementById('imageTwo');
+    productImageTwo.src = productImages[y].filePath;
+}
+var imageAppearThree = function() {
+    var z = productRandomizer();
+    var productImageThree = document.getElementById('imageThree');
+    productImageThree.src = productImages[z].filePath;
 }
 
-//productImageOne.addEventListener('click', handleClickonProduct);
-
-/*function handleClickonProduct() {
-  productClick += 1;
-  console.log('This product was clicked ' + productClick + ' times');
-}*/
+// var productClickOne = 0;
+// productImageOne.addEventListener('click', handleClickonOne);
+//
+// function handleClickonOne() {
+//   productClickOne += 1;
+//   console.log(this.Product + ' was clicked ' + productClickOne + ' times');
+// }
 
 var bag = new Products ('Star Wars Luggage', 'img/bag.jpg');
 var banana = new Products ('Banana Slicer', 'img/banana.jpg');
 var boots = new Products ('Toe-less Rain Boots', 'img/boots.jpg');
 var chair = new Products ('Chair', 'img/chair.jpg');
+var cthulu = new Products ('Cthulu Toy', 'img/cthulhu.jpg');
+var dragon = new Products ('Dragon Meat', 'img/dragon.jpg');
+var pen = new Products ('Untensil Pens', 'img/pen.jpg');
+var scissors = new Products ('Pizza Scissors', 'img/scissors.jpg');
 
-//handleClickonProduct();
-imageAppear();
+// handleClickonOne();
+imageAppearOne();
+imageAppearTwo();
+imageAppearThree();
