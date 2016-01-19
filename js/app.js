b01c7b14598bfcc239166de929a63848724f826a
icarrera <irismcarrera@gmail.com>
2016-01-19 14:59:13 -0800

    indicates which products were clicked

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ var productImages = [];
 function Products (productName, filePath) {
     this.productName = productName;
     this.filePath = filePath;
-    // timesClicked = 0;
-    // timesDisplayed = 0;
+    this.clickTotal = 0;
+    this.timesDisplayed = 0;
     // percentClicked = 0;
     // this.findPercentClick = function (){
     //   ((this.timesClicked/this.timesDisplayed)/ this.TotalClicks);
@@ -19,69 +19,75 @@ var boots = new Products('Toe-less Rain Boots', 'img/boots.jpg');
 var chair = new Products('Chair', 'img/chair.jpg');
 var cthulu = new Products('Cthulu Toy', 'img/cthulhu.jpg');
 var dragon = new Products('Dragon Meat', 'img/dragon.jpg');
-var pen = new Products('Untensil Pens', 'img/pen.jpg');
-var scissors = new Products('Pizza Scissors', 'img/scissors.jpg');
-var shark = new Products('Shark Sleeping Bag', 'img/shark.jpg');
-var sweep = new Products('Baby Broom Onesie', 'img/sweep.png');
-var unicorn = new Products('Unicorn Meat', 'img/unicorn.jpg');
-var usb = new Products('Tentacle USB', 'img/usb.gif');
-var waterCan = new Products('Watering Can', 'img/water-can.jpg');
-var wineGlass = new Products('Wine Glass', 'img/wine-glass.jpg');
+// var pen = new Products('Untensil Pens', 'img/pen.jpg');
+// var scissors = new Products('Pizza Scissors', 'img/scissors.jpg');
+// var shark = new Products('Shark Sleeping Bag', 'img/shark.jpg');
+// var sweep = new Products('Baby Broom Onesie', 'img/sweep.png');
+// var unicorn = new Products('Unicorn Meat', 'img/unicorn.jpg');
+// var usb = new Products('Tentacle USB', 'img/usb.gif');
+// var waterCan = new Products('Watering Can', 'img/water-can.jpg');
+// var wineGlass = new Products('Wine Glass', 'img/wine-glass.jpg');
 
 var imgRandom = function () {
     return Math.floor(Math.random() * productImages.length);
 }
 
-// var alreadyShown = [];
-// var img1 = 0;
-// var img2 =0;
-// var img3 = 0;
+var img1;
+var img2;
+var img3;
+
+//function that makes images appear
 var imageAppear = function(){
     var productImageOne = document.getElementById('imageOne');
     var productImageTwo = document.getElementById('imageTwo');
     var productImageThree = document.getElementById('imageThree');
-    var img1 = imgRandom();
+    img1 = imgRandom();
     productImageOne.src = productImages[img1].filePath;
-    var img2 = imgRandom();
+    productImages[img1].timesDisplayed ++;
+    img2 = imgRandom();
     while (img1 === img2) {
         img2 = imgRandom();
     };
     productImageTwo.src = productImages[img2].filePath;
-    var img3 = imgRandom();
+    productImages[img2].timesDisplayed ++;
+    img3 = imgRandom();
     while (img1 === img2 || img2 === img3 || img3 === img1) {
         img3 = imgRandom();
     };
     productImageThree.src = productImages[img3].filePath;
+    productImages[img3].timesDisplayed ++;
 
 }
 imageAppear();
-var productImageOne = document.getElementById('imageOne');
-var productImageTwo = document.getElementById('imageTwo');
-var productImageThree = document.getElementById('imageThree');
 
 var firstClick = 0;
 var secondClick = 0;
 var thirdClick = 0;
-// var totalClicks = 0;
+
+var productImageOne = document.getElementById('imageOne');
+var productImageTwo = document.getElementById('imageTwo');
+var productImageThree = document.getElementById('imageThree');
 
 function handleClickFirst(){
-    imageAppear();
-    firstClick++;
+    firstClick += 1;
     console.log('First was clicked ' + firstClick + ' times');
-  }
+    productImages[img1].clickTotal += 1;
+    imageAppear();
+}
 
 function handleClickSecond(){
-      imageAppear();
-      secondClick++;
+      secondClick += 1;
       console.log('Second was clicked ' + secondClick + ' times');
+      productImages[img2].clickTotal += 1;
+      imageAppear();
     }
 
 function handleClickThird(){
-        imageAppear();
-        thirdClick++;
+        thirdClick += 1;
         console.log('Third was clicked ' + thirdClick + ' times');
+        productImages[img3].clickTotal += 1;
+        imageAppear();
       }
-
 
 imageOne.addEventListener('click', handleClickFirst);
 imageTwo.addEventListener('click', handleClickSecond);
